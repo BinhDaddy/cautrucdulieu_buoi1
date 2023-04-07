c357962f9bb3d8c97a2fe77ac337a7a1485956cc
BinhDaddy <125379126+BinhDaddy@users.noreply.github.com>
2023-04-07 07:45:50 +0700

    Create CTDL&GT_Selection_Sort_1.cpp

---
+++ CTDL&GT_Selection_Sort_1.cpp
@@ -0,0 +1,11 @@
+cài đặt của phương pháo chọn trực tiép(selection Sort):
+void selectionSort(int arr[], int n) {
+   int i, j, min_idx;
+   for (i = 0; i < n-1; i++) {
+      min_idx = i;
+      for (j = i+1; j < n; j++)
+         if (arr[j] < arr[min_idx])
+            min_idx = j;
+      swap(arr[min_idx], arr[i]);
+   }
+}

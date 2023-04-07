5544863b88e49d4733797a5edb450a4599812205
BinhDaddy <125379126+BinhDaddy@users.noreply.github.com>
2023-04-07 10:24:05 +0700

    Update CTDL&GT_Heap_Sort_1.cpp

--- CTDL&GT_Heap_Sort_1.cpp
+++ CTDL&GT_Heap_Sort_1.cpp
@@ -37,22 +37,4 @@ void heapSort(int arr[], int n) {
         arr[0] = arr[i];
         arr[i] = temp;
         heapify(arr, i, 0);}}
-int main() {
-    int n;
-    printf("Nhap so phan tu cua day: ");
-    scanf("%d", &n);
-    int arr[n];
-    printf("Nhap day so: ");
-// Nhập dãy số từ bàn phím
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-// Sắp xếp dãy số bằng cách sử dụng heap sort
-    heapSort(arr, n);
-    printf("Day so da sap xep: ");
-// In dãy số đã được sắp xếp từ lớn đến nhỏ
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    return 0;
-}
+

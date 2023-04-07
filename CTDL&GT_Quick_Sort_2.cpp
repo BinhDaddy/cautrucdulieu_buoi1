10b85e13eeb4fa7542d9b0d102b4600a3cbb01e2
BinhDaddy <125379126+BinhDaddy@users.noreply.github.com>
2023-04-07 08:21:36 +0700

    Update CTDL&GT_Quick_Sort_2.cpp

--- CTDL&GT_Quick_Sort_2.cpp
+++ CTDL&GT_Quick_Sort_2.cpp
@@ -1,64 +1,56 @@
-ví 
-#include<stdio.h>
-#include<iostream>
+ví dụ
+#include <iostream>
 using namespace std;
- 
-//tạo hàm swap để tráo đổi các vị trí
-void swap(int &a, int &b)
-{
-    int t = a;
+
+// Hàm hoán đổi giá trị của hai phần tử
+void swap(int &a, int &b) {
+    int temp = a;
     a = b;
-    b = t;
+    b = temp;
 }
-  
-//  phân đoạn trong mảng
-int partition (int arr[], int low, int high)
-{
-    int pivot = arr[high];    // khai báo phần tử đánh dâu pivot
-    int left = low;   //khai báo biến bên trái
-    int right = high - 1; //khai báo biến bên phải
-    while(true){
-        while(left <= right && arr[left] < pivot) left++; // tìm phần tử >= phần tử pivot trong mảng
-        while(right >= left && arr[right] > pivot) right--; // tìm phần tử <= phần tử pivot trong mảng
-        if (left >= right) break; // sau khi duyệt xong thì thoát khỏi vòng lặp
-        swap(arr[left], arr[right]); // nếu chưa xong thì sử dụng hàm swap() để tráo đổi.
-        left++; // Vì left hiện tại đã xét, nên cần tăng
-        right--; // Vì right hiện tại đã xét, nên cần giảm
+
+// Hàm lấy pivot và phân hoạch mảng
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Chọn pivot là phần tử cuối cùng của mảng
+    int i = low - 1; // Đặt vị trí ban đầu của i là low - 1
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
     }
-    swap(arr[left], arr[high]);
-    return left; // Trả về chỉ số sẽ dùng để chia đôi mảng
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1; // Trả về vị trí của pivot sau khi phân hoạch
 }
-  
-/* Hàm thực hiện giải thuật quick sort */
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        // index là chỉ số nơi phần tử này đã đứng đúng vị trí và đây là phần tử chia mảng làm 2 mảng con trái & phải 
-        int index = partition(arr, low, high);
-  
-        // Gọi đệ quy sắp xếp 2 mảng con trái và phải
-        quickSort(arr, low, index - 1);
-        quickSort(arr, index + 1, high);
+
+// Hàm đệ quy thực hiện Quick Sort
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high); // Phân hoạch mảng và lấy vị trí của pivot
+
+        // Đệ quy thực hiện Quick Sort trên các phần mảng con trái và phải của pivot
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
     }
 }
-  
-/* Hàm xuất mảng */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++){
-        cout << arr[i];
-        cout<<"\t";
-      }
-}
-   
-int main()
-{
-    int arr[] = {9, -3, 5, 2, 6, 8, -6, 1, 3};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
-   
-    cout<<"Mảng sau khi được sắp xếp: \n";
-    printArray(arr, n); 
+
+int main() {
+    int arr[] = {7, 2, 1, 6, 8, 5, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Mang truoc khi sap xep: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "\nMang sau khi sap xep: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
